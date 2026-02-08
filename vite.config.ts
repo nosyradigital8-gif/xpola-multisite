5a02a0a9d6f598b047c4381625d927a75321c99c
nosyradigital8-gif <nosyradigital8@gmail.com>
2026-02-08 01:04:42 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+// FILE PATH: vite.config.ts
+// Place this file at: vite.config.ts (root of project)
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
